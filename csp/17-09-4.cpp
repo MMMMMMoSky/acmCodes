80dfff678c2c8049c7a85a86812aca05202165c9
RealSkyo <q94354906@gmail.com>
2018-03-03 11:18:35 +0800

    first written, sample WA, bug in AL found

---
+++ csp/17-09-4.cpp
@@ -0,0 +1,139 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+using namespace std;
+
+int n, m, N, cnt; // cnt [maybe bug]
+int pre[1005], sub[1005];
+int size[1005], belong[1005], dfn[1005], low[1005]; 
+bool instack[1005], hasWayIn[1005], added[1005][1005];
+stack <int> S;
+
+struct AdjacencyList {
+    int h[1005], nx[10005], to[10005];
+    int now, cnt;
+
+    AdjacencyList() {
+        memset(h, 0, sizeof h);
+        memset(nx, 0, sizeof nx);
+        memset(to, 0, sizeof to);
+        cnt = 0;
+    }
+    int first(int node) {
+        now = h[node];
+        return to[now];
+    }
+    int nxt() {
+        now = nx[now];
+        return to[now];
+    }
+    void addedge(int u, int v) {
+        to[++cnt] = v;
+        nx[cnt] = h[u];
+        h[u] = cnt;
+    }
+}G, T;
+
+void readInData()
+{
+    scanf("%d%d", &n, &m);
+    for(int i = 1; i <= m; i++)
+    {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        G.addedge(u, v);    
+    }
+}
+
+void tarjan(int u)
+{
+    dfn[u] = low[u] = ++cnt;
+    S.push(u);
+    instack[u] = 1;
+    for(int v = G.first(u); v; v = G.nxt())
+    {
+        if(!dfn[v]) {
+            printf("%d --> %d\n", u, v);
+            tarjan(v);
+        }
+        if(instack[v]) low[u] = min(low[u], low[v]);
+    }
+    if(dfn[u] == low[u]) {
+        N++;
+        int t;
+        do {
+            t = S.top();
+            S.pop();
+            instack[t] = 0;
+            belong[t] = N;
+            size[t]++;
+        }while(t != u);
+    }
+}
+
+void buildT(int u)
+{
+    int bu = belong[u], bv;
+    instack[u] = 1;
+    for(int v = G.first(u); v; v = G.nxt())
+    {
+        bv = belong[v]; 
+        if(!instack[v]) {
+            printf("%d --> %d\n", u, v);
+            buildT(v);
+        }
+        if(bu != bv && !added[bu][bv]) {
+            T.addedge(bu, bv);
+            added[bu][bv] = 1;
+            hasWayIn[bv] = 1;
+        }     
+    }
+}
+
+void tarjanBuild()
+{
+    for(int i = 1; i <= n; i++)
+        if(!dfn[i]) tarjan(i);    
+    memset(instack, 0, sizeof instack); // used as vis[]
+    for(int i = 1; i <= n; i++)
+        if(!instack[i]) buildT(i);    
+}
+
+void dfs(int u)
+{
+    for(int v = T.first(u); v; v = T.nxt())
+    {
+        pre[v] += pre[u] + 1;
+        dfs(v);
+        sub[u] += sub[v] + 1; 
+    }
+}
+
+void print(int x)
+{
+    for(int i = 1; i <= n; i++)
+        if(belong[i] == x) printf("%d ", i);
+    putchar('\n');
+}
+
+void solve()
+{
+    int ans = 0;
+    for(int i = 1; i <= N; i++)
+        if(!hasWayIn[i]) dfs(i);
+    for(int i = 1; i <= N; i++) 
+        if(sub[i]+pre[i] == N-1) {
+            ans += size[i];
+            //print(i);
+        }
+    printf("%d\n", ans);
+}
+
+int main()
+{
+    readInData();
+    tarjanBuild();
+    solve();
+    return 0;
+}
